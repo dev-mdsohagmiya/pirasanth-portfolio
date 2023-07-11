1f7486be77f45b787d648d21f0ac42f17fa9affc
ms-msbhh-ms <shohag.i.sajjad@gmail.com>
2023-07-11 23:37:35 +0600

    update hero section text animation

---
+++ components/AnimatedHeroText.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import TextAutomation from "./TextAutomation";
+import AnimatedText from "react-animated-text-content";
+const AnimatedHeroText = () => {
+  return (
+    <div className="md:translate-x-96 p-3 w-full">
+      <div className=" ">
+        <div className="">
+          <div className="">
+            <div className="text-primary block text-[40px] md:text-[68px] lg:text-[70px] xl:text-[70px]">
+              <AnimatedText
+                type="words" // animate words or chars
+                animation={{
+                  x: "200px",
+                  y: "-20px",
+                  scale: 1.1,
+                  ease: "ease-in-out",
+                }}
+                animationType="float"
+                interval={0.06}
+                duration={0.8}
+                tag="p"
+                includeWhiteSpaces
+                threshold={0.1}
+                rootMargin="20%"
+              >
+                Hi!
+              </AnimatedText>
+            </div>
+          </div>
+          <p className="text-[20px] md:text-[28px] lg:text-[32px] pt-6 lg:pt-14 text-white">
+            I'm <span className="text-primary">Pirasanth</span> - but call me
+            <span className="text-primary">PJ.</span>
+          </p>
+        </div>
+        <div className="">
+          <span className="text-[20px] md:text-[28px] lg:text-[32px] pt-3 lg:pt-8 text-white block ">
+            <div>
+              <div>
+                <AnimatedText
+                  type="words" // animate words or chars
+                  animation={{
+                    x: "200px",
+                    y: "-20px",
+                    scale: 1.1,
+                    ease: "ease-in-out",
+                  }}
+                  animationType="float"
+                  interval={0.1}
+                  duration={0.8}
+                  tag="p"
+                  className="animated-paragraph md:text-[29px] text-[19px]"
+                  includeWhiteSpaces
+                  threshold={0.3}
+                  rootMargin="10%"
+                >
+                  {`I’m a frontend developer and`}
+                </AnimatedText>
+                <AnimatedText
+                  type="words" // animate words or chars
+                  animation={{
+                    x: "200px",
+                    y: "-20px",
+                    scale: 1.1,
+                    ease: "ease-in-out",
+                  }}
+                  animationType="float"
+                  interval={0.1}
+                  duration={0.8}
+                  tag="p"
+                  className="animated-paragraph md:text-[29px] text-[19px]"
+                  includeWhiteSpaces
+                  threshold={0.3}
+                  rootMargin="10%"
+                >
+                  {`automation QA living in London.`}
+                </AnimatedText>
+              </div>
+            </div>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnimatedHeroText;
--- sections/hero.tsx
+++ sections/hero.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import AnimatedText from "../components/AnimatedText";
+import AnimatedHeroText from "../components/AnimatedHeroText";
 import "../styles/globals.css";
+
 function Hero() {
   return (
     <section id="hero">
       <div className="md:grid gap-4 grid-cols-12 max-w-7xl mx-auto lg:px-5 ">
         <div className="relative lg:py-20 col-span-12  lg:col-span-10">
-          <div className="rounded-lg my-10 p-1 lg:p-0 bg-secondary-shaded md:absolute md:-left-96 md:transform">
+          <div className="rounded-lg my-10 p-1 lg:p-0 bg-secondary-shaded md:absolute md:-left-96  md:transform">
             <div className="grid grid-cols-12  ">
               <div
                 className="col-span-12 md:col-span-12    pt-2 md:pt-36 pb-2 md:pb-20   rounded-lg  
                   flex justify-end"
               >
-                <AnimatedText />
+                <AnimatedHeroText />
               </div>
               <div className="col-span-4 w-96"></div>
             </div>
